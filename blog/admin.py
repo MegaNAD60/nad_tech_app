9610b64428968c8d420fb1d5d5c5afc5baa03327
Nehemiah <nehemiahdauda60@gmail.com>
2022-09-14 23:40:10 +0100

    modified web pages

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, PostImage
 
-admin.site.register(Post)
+class PostImageAdmin(admin.StackedInline):
+    model = PostImage
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = [PostImageAdmin]
+
+    class Meta:
+        model = Post
+
+@admin.register(PostImage)
+class PostImageAdmin(admin.ModelAdmin):
+    pass
+
+
 # Register your models here.
